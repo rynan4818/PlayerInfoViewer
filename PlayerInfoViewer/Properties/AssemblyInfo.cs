894234654584c868a6843d3fe9fd1d99772852d6
Loloppe <LaurentBlaisCote@outlook.com>
2024-06-06 19:35:45 -0400

    BS 1.37 update

--- PlayerInfoViewer/Properties/AssemblyInfo.cs
+++ PlayerInfoViewer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PlayerInfoViewer")]
-[assembly: AssemblyCopyright("Copyright ©  2022 Rynan4818")]
+[assembly: AssemblyCopyright("Copyright ©  2024 Rynan4818")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.2")]
-[assembly: AssemblyFileVersion("0.4.2")]
+[assembly: AssemblyVersion("0.4.3")]
+[assembly: AssemblyFileVersion("0.4.3")]
